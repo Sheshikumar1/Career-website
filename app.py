60464902f71cf107c2272ceee4b469b8a8a2030f
sheshikumar1 <sheshikumarerraboina@gmail.com>
2024-07-08 23:26:50 +0530

    added jobs

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
 from flask import Flask,render_template,request
 app=Flask(__name__)
 
+JOBS=[
+    {
+        'id':1,
+        'title': 'Data Analyst',
+        'salary':'Rs.15,000,00',
+        'location':'Delhi India'
+    },
+    {
+        
+        'id':2,
+        'title': 'Data Engineer',
+        'salary':'Rs.25,000,00',
+        'location':'Hyderabad India'
+    
+    },
+    {
+        'id':3,
+        'title': 'Busniess Analyst',
+        'salary':'Rs.15,000,00',
+        'location':'Munbia India'
+    },
+    {
+        'id':4,
+        'title': 'Web Developer',
+        'salary':'Rs.10,000,00',
+        'location':'Remote'
+    }
+
+]
 @app.route('/')
 def home():
-    return render_template('hello.html')
+    return render_template('hello.html',jobs=JOBS)
 
 if __name__=='__main__':
     app.run(debug=True)
